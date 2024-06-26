4e595c78cbe6935701783a68f9e9aa90bf4c525c
Aditya Shibu <as2397@hw.ac.uk>
2024-06-26 22:28:00 +0400

    Added all base files

---
+++ forms/contact.php
@@ -0,0 +1,42 @@
+<?php
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize the input data
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = trim($_POST["subject"]);
+    $message = trim($_POST["message"]);
+ 
+    // Set the recipient email address
+    $recipient = "kiran.narayana@clarus24.com";
+ 
+    // Set the email subject
+    $subject = "New contact from $name: $subject";
+ 
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Subject: $subject\n";
+    $email_content .= "Message:\n$message\n";
+ 
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+ 
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Set a 500 (internal server error) response code
+        http_response_code(500);
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+ 
+} else {
+    // Not a POST request, set a 403 (forbidden) response code
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
