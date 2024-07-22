0d860562ed45ab3a0db53bf64e304374616b88db
Aditya Shibu <as2397@hw.ac.uk>
2024-07-22 12:44:34 +0400

    Changed contact name

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Contact@Clarus24",
+          to_name: "Aadarsh",
           from_email: form.email,
           to_email: "contact@clarus24.com",
           message: form.message,
