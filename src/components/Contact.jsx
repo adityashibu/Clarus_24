3e86439b6583d9866b184943aa108514f380d0fe
Aditya Shibu <as2397@hw.ac.uk>
2024-07-22 12:34:42 +0400

    Updated contact to use contact email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Aditya Shibu",
+          to_name: "Aadarsh",
           from_email: form.email,
-          to_email: "adityashibuonline@gmail.com",
+          to_email: "contact@clarus24.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
