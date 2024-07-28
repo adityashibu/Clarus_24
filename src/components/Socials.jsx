fb551346d62096ec568507a1cf9c41ab2452c92e
Aditya Shibu <as2397@hw.ac.uk>
2024-07-28 16:07:48 +0400

    Added socials and env file

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -6,7 +6,7 @@ import { faInstagram, faLinkedin, faFacebook } from '@fortawesome/free-brands-sv
 const Socials = () => {
     return (
         <div className="flex space-x-4 mt-2 text-white">
-            <a href="https://www.instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/clarus24_consulting" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faInstagram} className="text-2xl hover:text-gray-300 px-2" />
             </a>
             <a href="https://www.linkedin.com/company/clarus24/" target="_blank" rel="noopener noreferrer">
